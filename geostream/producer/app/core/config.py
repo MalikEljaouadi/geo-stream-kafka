adcd13faac7aa8e1a544351c4b96c65be3eaec3a
MalekEljaouadi <malekabidi25@gmail.com>
2022-04-29 16:18:58 +0100

    set the config of the producer

--- geostream/producer/app/core/config.py
+++ geostream/producer/app/core/config.py
@@ -1,22 +1,26 @@
-import logging
 import sys
 
-from producer.app.core.logging import InterceptHandler
 from loguru import logger
-from starlette.config import Config
+from pydantic import BaseModel, Field
 
-config = Config(".env")
+import logging
+from producer.app.core.logging import InterceptHandler
 
 
-PROJECT_NAME: str = config("PROJECT_NAME", default="geostream-kafka-producer")
-KAFKA_URI: str = config("KAFKA_URI")
-KAFKA_PORT: str = config("KAFKA_PORT")
-KAFKA_INSTANCE = KAFKA_URI + ":" + KAFKA_PORT
-DEBUG: bool = config("DEBUG", cast=bool, default=False)
+class ProducerConfig(BaseModel):
+    PROJECT_NAME: str = Field(default="geostream-kafka-producer", env="PROJECT_NAME")
+    KAFKA_URI: str = Field(default="0.0.0.0", env="KAFKA_URI")
+    KAFKA_PORT: str = Field(default="9092", env="KAFKA_PORT")
+    DEBUG: bool = Field(default=False, env="DEBUG")
 
-LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
 
-logging.basicConfig(
-    handlers=[InterceptHandler(level=LOGGING_LEVEL)], level=LOGGING_LEVEL
-)
+# Initialize the ProducerConfig class
+producer_config = ProducerConfig()
+
+# Kafka_broker URL
+KAFKA_INSTANCE = producer_config.KAFKA_URI + ":" + producer_config.KAFKA_PORT
+
+# Logging setup
+LOGGING_LEVEL = logging.DEBUG if producer_config.DEBUG else logging.INFO
+logging.basicConfig(handlers=[InterceptHandler(level=LOGGING_LEVEL)], level=LOGGING_LEVEL)
 logger.configure(handlers=[{"sink": sys.stderr, "level": LOGGING_LEVEL}])
